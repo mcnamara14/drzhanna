3c5431fac5e3ec11aab6d21d654428905791a49c
Tyler McNamara <tyler.mcnamara@housecallpro.com>
2020-08-07 12:43:45 -0700

    update gallery

--- src/templates/ThisDocMakesHouseCalls/RenovationGallery/renovationGalleryData.js
+++ src/templates/ThisDocMakesHouseCalls/RenovationGallery/renovationGalleryData.js
@@ -11,10 +11,9 @@ import gallery4Image2 from '../../../images/renovation-gallery-3/image-2.jpg'
 import gallery4Image3 from '../../../images/renovation-gallery-3/image-3.jpg'
 
 const renovationGalleryData = [
-    { name: 'gallery1', images: [gallery1Image1, gallery1Image2] },
-    { name: 'gallery2', images: [gallery2Image1, gallery2Image2, gallery2Image3] },
-    { name: 'gallery3', images: [gallery3Image1, gallery3Image2, gallery3Image3] },
-    { name: 'gallery4', images: [gallery4Image1, gallery4Image2, gallery4Image3] }
+    { name: 'gallery1', images: [gallery2Image1, gallery2Image2, gallery2Image3] },
+    { name: 'gallery2', images: [gallery3Image1, gallery3Image2, gallery3Image3] },
+    { name: 'gallery3', images: [gallery4Image1, gallery4Image2, gallery4Image3] }
 ]
 
 export default renovationGalleryData
