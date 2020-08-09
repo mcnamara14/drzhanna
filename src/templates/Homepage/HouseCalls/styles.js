147c4feb73dccbd00920156cb3f28072e06e6120
Tyler McNamara <tyler.mcnamara@housecallpro.com>
2020-08-08 21:38:45 -0700

    update

--- src/templates/Homepage/HouseCalls/styles.js
+++ src/templates/Homepage/HouseCalls/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Col } from 'antd'
 
 const Container = styled.div`
-    margin: 100px 0 60px 0;
+    margin: 150px 0 60px 0;
 
     &:hover {
         cursor: pointer;
