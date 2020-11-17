b0ec7ed6feff2e8ed098ed1b1af386f3a1c73092
Tyler McNamara <tyler.mcnamara@housecallpro.com>
2020-11-16 21:06:51 -0800

    updates

--- src/components/MailChimpModal/index.js
+++ src/components/MailChimpModal/index.js
@@ -47,22 +47,24 @@ const MailChimpModal = () => {
         <Row>
           <Col xs={12} className="mail-chimp-modal--image" />
           <Col xs={12} className="mail-chimp-modal--form">
-            <h2>Win a Free copy of Dr. Zhanna’s New Recipe Book!</h2>
+            <h2>Win a Free copy of Dr. Zhanna’s New Recipe Book <span><span>By</span> The Numbers</span> !</h2>
             <p>Submit your name and email address to receive Dr. Zhanna’s new book, By the Numbers, for free.</p>
-            <Mailchimp
-              action='https://drzhanna.us2.list-manage.com/subscribe/post?u=863a065d37813a158479e0f3d&amp;id=42565cb0c8'
-              fields={fields}
-              messages = {
-                {
-                  sending: "Sending...",
-                  success: "Thank you for subscribing!",
-                  error: "An unexpected internal error has occurred.",
-                  empty: "You must write an e-mail.",
-                  duplicate: "Too many subscribe attempts for this email address",
-                  button: "SUBMIT"
+            <div className="mail-chimp-modal--form-fields">
+              <Mailchimp
+                action='https://drzhanna.us2.list-manage.com/subscribe/post?u=863a065d37813a158479e0f3d&amp;id=42565cb0c8'
+                fields={fields}
+                messages = {
+                  {
+                    sending: "Sending...",
+                    success: "Thank you for subscribing!",
+                    error: "An unexpected internal error has occurred.",
+                    empty: "You must write an e-mail.",
+                    duplicate: "Too many subscribe attempts for this email address",
+                    button: "ENTER FOR A CHANCE TO WIN"
+                  }
                 }
-              }
-            />
+              />
+            </div>
           </Col>
         </Row>
      </Container>
--- src/components/MailChimpModal/styles.js
+++ src/components/MailChimpModal/styles.js
@@ -2,11 +2,14 @@ import styled from 'styled-components'
 import { Modal } from 'antd';
 
 const Container = styled(Modal)`
-  height: 500px;
 
   .mail-chimp-modal--image {
     background: ${({ bgImage}) => `url(${bgImage})`};
     background-size: cover;
+  }
+
+  .mail-chimp-modal--form-fields {
+    margin-top: 25px;
   }
 
   .ant-modal-content {
@@ -23,17 +26,16 @@ const Container = styled(Modal)`
 
   .ant-modal-body {
     padding: 0;
-    height: 500px;
   }
 
   .mail-chimp-modal--form {
-    padding: 20px;
+    padding: 20px 60px 20px 20px;
 
     button {
       background: #d0a72d;
       border: none;
       height: 45px;
-      width: auto;
+      width: 100%;
       text-shadow: 0 -1px 0 rgba(0,0,0,.12);
       box-shadow: 0 2px 0 rgba(0,0,0,.045);
       color: white;
@@ -57,9 +59,36 @@ const Container = styled(Modal)`
 
     input {
       border: 1px solid #eee;
+      border-radius: 3px;
       width: 100%;
-      margin-bottom: 20px;
+      margin-bottom: 10px;
       padding: 10px;
+      display: block;
+    }
+
+    h2 {
+      font-family: "Playfair Display";
+      font-weight: 500;
+      line-height: 140%;
+      font-size: 36px;
+      margin-bottom: 10px;
+    }
+
+    p {
+      font-family: Montserrat;
+      font-size: 14px;
+      line-height: 22px;
+    }
+
+    span {
+      font-family: Landscape Land;
+      font-size: 46px;
+      color: #d0a72d;
+      margin: 0;
+
+      span {
+        letter-spacing: -7px;
+      }
     }
   }
     
