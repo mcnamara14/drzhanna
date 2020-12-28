0e8dba692187a72f9785070d1fdd971b40b0f042
Tyler McNamara <>
2020-12-27 20:25:47 -0800

    add google tag manager code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-}
+};
 
 // if you want to use the preview API please define
 // CONTENTFUL_HOST in your environment config
 // the `host` property should map to `preview.contentful.com`
 // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
 if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
 }
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
     'Contentful spaceId and the access token need to be provided.'
-  )
+  );
 }
 
 module.exports = {
@@ -41,29 +41,37 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Playfair Display\:400, 500, 600,700`, 
-          `Montserrat\:100, 200, 300, 400, 500, 600, 700`, 
-          `Quicksand\: 300, 500, 700`], 
-          display: 'swap'
+          `Playfair Display\:400, 500, 600,700`,
+          `Montserrat\:100, 200, 300, 400, 500, 600, 700`,
+          `Quicksand\: 300, 500, 700`,
+        ],
+        display: 'swap',
       },
     },
     {
-      resolve: `gatsby-plugin-styled-components`, 
+      resolve: `gatsby-plugin-styled-components`,
     },
     {
-      resolve: "gatsby-plugin-web-font-loader",
+      resolve: 'gatsby-plugin-web-font-loader',
       options: {
         custom: {
-          families: ["Landscape Land"],
-          urls: ["/fonts/fonts.css"],
+          families: ['Landscape Land'],
+          urls: ['/fonts/fonts.css'],
         },
       },
     },
     {
       resolve: `gatsby-plugin-facebook-pixel`,
       options: {
-        pixelId: "208665977542542",
+        pixelId: '208665977542542',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'G-QLPXBPG691',
+        includeInDevelopment: false,
       },
     },
   ],
-}
+};
