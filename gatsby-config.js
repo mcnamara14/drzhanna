7b462afeba0a7908fb56841837fda93ef0afe7b7
Tyler McNamara <>
2021-01-16 18:16:53 -0800

    add google analytics and buy book button

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,10 +67,12 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        id: 'G-QLPXBPG691',
-        includeInDevelopment: false,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-QLPXBPG691', // Google Analytics / GA
+        ],
       },
     },
   ],
